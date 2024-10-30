c88ca84f8a88c349a561edefc318ca9f5ed5927b
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-30 18:41:02 +0100

    add img

--- app.js
+++ app.js
@@ -4,35 +4,36 @@ const port = 3000;
 const host = "http://127.0.0.1";
 
 
+app.use(express.static('public'));
 const posts = [
     {
         titolo :  "titolo_1",
         contenuto : "contenuto_1",
-        immagine : "placeholder-image1.jpg",
+        immagine : "img/img1.png",
         tags: ["tag_1", "tag_2"]
     },
     {
         titolo :  "titolo_2",
         contenuto : "contenuto_2",
-        immagine : "placeholder-image2.jpg",
+        immagine : "./img/img2.png",
         tags: ["tag_3", "tag_4"]
     },
     {
         titolo :  "titolo_3",
         contenuto : "contenuto_3",
-        immagine : "placeholder-image3.jpg",
+        immagine : "/img/img3.png",
         tags: ["tag_5", "tag_6"]
     },
     {
         titolo :  "titolo_4",
         contenuto : "contenuto_4",
-        immagine : "placeholder-image4.jpg",
+        immagine : "./img/img4.png",
         tags: ["tag_7", "tag_8"]
     },
     {
         titolo :  "titolo_5",
         contenuto : "contenuto_5",
-        immagine : "placeholder-image5.jpg",
+        immagine : "./img/img5.png",
         tags: ["tag_9", "tag_10"]
     },
 ];
