d2aabb60d6611bef314040d6f93387dcafb58af0
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-30 16:32:03 +0100

    array posts

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const host = "http://127.0.0.1"
+const host = "http://127.0.0.1";
+
+const posts = [
+    {
+        titolo :  "titolo",
+        contenuto : "contenuto",
+        immagine : "placeholder-image1.jpg",
+        tags: ["tag_1", "tag_2"]
+    },
+]
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Benvenuto nel mio blog!</h1>")
@@ -9,3 +19,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/posts", (req, res) =>{
+    res.json("")
+})
