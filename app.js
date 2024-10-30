ea9f2764f62cd3023822f6d5f57040f667df3197
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-30 16:07:13 +0100

    progetto base

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "http://127.0.0.1"
+
+app.get("/", (req, res) => {
+    res.send("<h1>Benvenuto nel mio blog!</h1>")
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
